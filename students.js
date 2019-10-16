4ca51938bd008f43707d24bd22a855a9be6fa0bf
jomicm <singularity.jmcm@gmail.com>
2019-10-16 12:48:23 -0400

    Add parameterized queries to files

--- students.js
+++ students.js
@@ -1,8 +1,9 @@
+const { Pool } = require('pg');
 const args = process.argv.splice(2);
 
 const $cohortName = args[0];
-const $queryLimit = args[1];
-const { Pool } = require('pg');
+const $queryLimit = args[1] || 5;
+const values = [`%${$cohortName}%`, $queryLimit];
 
 const pool = new Pool({
   user: 'miguelcruz',
@@ -11,16 +12,16 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-  SELECT s.id, s.name student_name, c.name cohort_name
-  FROM students s
-  JOIN cohorts c
-  ON s.cohort_id=c.id
-  WHERE c.name LIKE '%${$cohortName}%'
-  LIMIT ${$queryLimit};
-`)
+const queryString = `
+SELECT s.id, s.name student_name, c.name cohort_name
+FROM students s
+JOIN cohorts c
+ON s.cohort_id=c.id
+WHERE c.name LIKE $1
+LIMIT $2
+`;
+pool.query(queryString, values)
   .then(res => {
     res.rows.map(res => console.log(`${res.student_name} has an id of ${res.id} and was in the ${res.cohort_name} cohort`));
   })
   .catch(err => console.error('query error', err.stack));
-
--- teachers.js
+++ teachers.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
 const args = process.argv.splice(2);
 const $cohortName = args[0];
+const values = [$cohortName];
 
 const pool = new Pool({
   user: 'miguelcruz',
@@ -9,17 +10,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-  SELECT DISTINCT t.name student_name, c.name cohort_name
-  FROM teachers t
-  JOIN assistance_requests a
-  ON t.id = a.teacher_id
-  JOIN students s
-  ON a.student_id=s.id
-  JOIN cohorts c
-  ON c.id=s.cohort_id
-  WHERE c.name='${$cohortName}';
-`)
+const queryString = `
+SELECT DISTINCT t.name student_name, c.name cohort_name
+FROM teachers t
+JOIN assistance_requests a
+ON t.id = a.teacher_id
+JOIN students s
+ON a.student_id=s.id
+JOIN cohorts c
+ON c.id=s.cohort_id
+WHERE c.name=$1;
+`;
+pool.query(queryString, values)
   .then(res => {
     res.rows.map(res => console.log(`${res.cohort_name}: ${res.student_name}`));
   })
